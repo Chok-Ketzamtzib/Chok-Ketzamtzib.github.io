fe6aa342ce910697cc7473af4cbe3e449bf71b4b
Chok-Ketzamtzib <wakeforest.ww@gmail.com>
2019-09-06 11:16:37 -0400

    Updates

---
+++ precache-manifest.fefbf09bab042da0281ab5e887bbbed8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "675a96af3ea1b3100af750e4565a2d95",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "cd0c30db4753aaa567c0",
+    "url": "/react-portfolio/static/css/main.e8986148.chunk.css"
+  },
+  {
+    "revision": "f8f83564bb6f729a1929",
+    "url": "/react-portfolio/static/js/2.7baaa794.chunk.js"
+  },
+  {
+    "revision": "cd0c30db4753aaa567c0",
+    "url": "/react-portfolio/static/js/main.49bfc4ac.chunk.js"
+  },
+  {
+    "revision": "8589cc06824bc596de76",
+    "url": "/react-portfolio/static/js/runtime~main.13b52e4d.js"
+  }
+]);
